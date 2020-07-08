6f498215a1b7c6c808f71761c68b77a41922e215
Wesley Wen <wwen@WS-30056-MAC.local>
2020-07-08 16:30:47 -0700

    Installed BeautifulSoup for webscraping and implemented POST validation checks

--- api/courses/courses.py
+++ api/courses/courses.py
@@ -3,6 +3,8 @@ from api.courses.courses_serializers import CourseListSerializer
 from course.models import Course, UserCourses
 from user.models import User
 from rest_framework.response import Response
+from requests import get
+from bs4 import BeautifulSoup
 
 
 class CourseList(generics.ListCreateAPIView):
@@ -22,43 +24,56 @@ class CourseList(generics.ListCreateAPIView):
         course = Course.objects.filter(subject_code=subject_code, subject_number=subject_number, section_number=section_number)
         user = User.objects.filter(phone_number=phone_number)
 
-        if course and user:
-            # if the course already exists in the database
+        url = f"https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-section&dept={subject_code}&course={subject_number}&section={section_number}"
+        response = get(url)
+
+        html_soup = BeautifulSoup(response.text, 'html.parser')
+        type(html_soup)
+        general_seat_div = html_soup.find_all(string='General Seats Remaining:')
+        general_seat_count = general_seat_div[0].findParent().findNextSibling().text
+        print('general_seat_count', general_seat_count)
+
+        if general_seat_count == '0':
+            if course and user:
+                # if the course already exists in the database
                 # Check if record already exists in the user table
-            unique_reservation = UserCourses.objects.filter(user=user[0].id, course=course[0].id)
-            # if unique_reservation:
-            #     return Response({'status': 'You have already registered for this course'})
-            reservation = UserCourses(user=user[0], course=course[0])
-            reservation.save()
-            return Response({'status': 'Success'})
 
-        elif not user and not course:
-            user_data = User(phone_number=phone_number)
-            user_data.save()
-            course_data = Course(subject_code=subject_code, subject_number=subject_number, section_number=section_number)
-            course_data.save()
-            course_data.users.set([user_data])
+                unique_reservation = UserCourses.objects.filter(user=user[0].id, course=course[0].id)
+                if unique_reservation:
+                    return Response({'status': 'You have already registered for this course'})
 
-            reservation = UserCourses(user=user_data, course=course_data)
-            reservation.save()
-            return Response({'status': 'Success'})
+                reservation = UserCourses(user=user[0], course=course[0])
+                reservation.save()
+                return Response({'status': 'Success'})
 
-        elif course and not user:
-            # if the phone number does not exist in the database yet
-            user_data = User(phone_number=phone_number)
-            user_data.save()
+            elif not user and not course:
+                user_data = User(phone_number=phone_number)
+                user_data.save()
+                course_data = Course(subject_code=subject_code, subject_number=subject_number, section_number=section_number)
+                course_data.save()
+                course_data.users.set([user_data])
 
-            reservation = UserCourses(user=user_data, course=course[0])
-            reservation.save()
-            return Response({'status': 'Success'})
+                reservation = UserCourses(user=user_data, course=course_data)
+                # reservation.save()
+                return Response({'status': 'Success'})
 
-        elif user and not course:
-            # if the course does not exist in the database yet
-            course_data = Course(subject_code=subject_code, subject_number=subject_number, section_number=section_number)
-            course_data.save()
-            course_data.users.set([user[0]])
+            elif course and not user:
+                # if the phone number does not exist in the database yet
+                user_data = User(phone_number=phone_number)
+                user_data.save()
 
-            reservation = UserCourses(user=user[0], course=course_data)
-            # reservation.save()
-            return Response({'status': 'Success'})
+                reservation = UserCourses(user=user_data, course=course[0])
+                reservation.save()
+                return Response({'status': 'Success'})
 
+            elif user and not course:
+                # if the course does not exist in the database yet
+                course_data = Course(subject_code=subject_code, subject_number=subject_number, section_number=section_number)
+                course_data.save()
+                course_data.users.set([user[0]])
+
+                reservation = UserCourses(user=user[0], course=course_data)
+                # reservation.save()
+                return Response({'status': 'Success'})
+
+        return Response({'status': 'The course you requested is not full'})
